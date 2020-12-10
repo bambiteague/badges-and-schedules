4a1d89852c3afd91166db8e965251ada84489caf
bambi teague <teeegsa@gmail.com>
2020-12-09 19:34:10 -0600

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,35 +1,103 @@
 
 # Create a method called printer that will output first the results of the 'batch_badge_creator' method
 # and then of 'the assign_rooms' method to the screen
-name = "Arel"
-attendees = "Edsger", "Ada", "Char[les", "Alan", "Grace", "Linus", "Matz"
+
 
 def badge_maker(name)
-   "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-    badges = []
-    attendees.each {|attendee| badges << "Hello, my name is #{attendee}."} 
-    badges
-end     
+  badges = []
+  attendees.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+  badges
+end
 
-def assign_rooms(attendees)
-    room_assignments =[]
-    attendees.each_with_index {|attendees, index| room_assignments << 
-         "Hello, #{attendees}! You'll be assigned to room #{index+1}!"}
-    room_assignments
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |speakers, index|
+    room_assignments << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
+  end
+  room_assignments
 end
 
 def printer(attendees)
-   batch_badge_creator(attendees).each do |attendee|
-     puts attendee
-   end
-   assign_rooms(attendees).each do |room|
-     puts room
-   end 
+  batch_badge_creator(attendees).each do |name|
+    puts name
+  end
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
+end
 
-end 
 
-#   badges_and_room_assignments.each_line do |line|     <--- saw in spec (will it work?)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def badge_maker(name)
+#    "Hello, my name is #{name}."
+# end
+
+# def batch_badge_creator(attendees)
+#     badges = []
+#     attendees.each {|attendee| badges << "Hello, my name is #{attendee}."} 
+#     badges
+# end     
+
+# def assign_rooms(attendees)
+#     room_assignments =[]
+#     attendees.each_with_index {|attendees, index| room_assignments << 
+#          "Hello, #{attendees}! You'll be assigned to room #{index+1}!"}
+#     room_assignments
+# end
+
+# def printer(attendees)
+#    batch_badge_creator(attendees).each do |attendee|
+#      puts attendee
+#    end
+#    assign_rooms(attendees).each do |room|
+#      puts room
+#    end 
+
+# end 
+
 
